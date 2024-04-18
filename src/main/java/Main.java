f0e57b5262895e3c1001146371f75602beaf130e
polanap <95124950+polanap@users.noreply.github.com>
2024-04-18 11:01:05 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,24 +4,19 @@ import ru.ifmo.se.pokemon.Pokemon;
 
 public class Main {
     public static void main(String[] args) {
-        int b =9;
-        int y = 0;
-        System.out.println(1);
-        double result = b/y;
-        System.out.println(result);
-        // Battle b = new Battle();
-        // Pokemon p1 = new Lapras("pokemons.Lapras", 2);
-        // Pokemon p2 = new Gorebyss("pokemons.Gorebyss", 1);
-        // Pokemon p3 = new Clamperl("pokemons.Clamperl", 1);
-        // Pokemon p4 = new Pichu("pokemons.Pichu", 1);
-        // Pokemon p5 = new Pikachu("pokemons.Pikachu", 1);
-        // Pokemon p6 = new Raichu("pokemons.Raichu", 1);
-        // b.addAlly(p1);
-        // b.addAlly(p2);
-        // b.addAlly(p3);
-        // b.addFoe(p4);
-        // b.addFoe(p5);
-        // b.addFoe(p6);
-        // b.go();
+        Battle b = new Battle();
+        Pokemon p1 = new Lapras("pokemons.Lapras", 2);
+        Pokemon p2 = new Gorebyss("pokemons.Gorebyss", 1);
+        Pokemon p3 = new Clamperl("pokemons.Clamperl", 1);
+        Pokemon p4 = new Pichu("pokemons.Pichu", 1);
+        Pokemon p5 = new Pikachu("pokemons.Pikachu", 1);
+        Pokemon p6 = new Raichu("pokemons.Raichu", 1);
+        b.addAlly(p1);
+        b.addAlly(p2);
+        b.addAlly(p3);
+        b.addFoe(p4);
+        b.addFoe(p5);
+        b.addFoe(p6);
+        b.go();
 }
 }
